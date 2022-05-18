e9c0814b7f141c35f4fbe011c332c5433210bbef
hacker228973 <kodiy9@gmail.com>
2022-05-18 20:05:59 +0200

    исправил полностью

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-
         boolean turn = true;
         do {
             System.out.println("Введите команду из списка предложенных");
             System.out.println("add, remove, exit");
+            Scanner scanner = new Scanner(System.in);
             switch (scanner.nextLine()) {
-                case "exit":
-                    turn = false;
-                    break;
                 case "add":
-
-                    Product.add(products);
+                    System.out.println("Введите название продукта");
+                    String nameAdd = scanner.nextLine();
+                    System.out.println("Введите кол-во продукта");
+                    int count = scanner.nextInt();
+                    System.out.println("Введите цену продукта");
+                    int price = scanner.nextInt();
+                    Product.add(products,nameAdd,count,price);
                     break;
                 case "remove":
-
-                    Product.remove(products);
+                    System.out.println("Введите название продукта");
+                    String nameRemove = scanner.nextLine();
+                    Product.remove(products,nameRemove);
                     break;
-
                 default:
                     turn = false;
                     break;
--- src/Product.java
+++ src/Product.java
@@ -1,17 +1,13 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Product {
     String name;
     int count;
     int price;
-
     public Product(String name, int count, int price) {
         this.name = name;
         this.count = count;
         this.price = price;
     }
-
     @Override
     public String toString() {
         return "\nname='" + name + ", count=" + count + ", price=" + price;
@@ -20,40 +16,21 @@ public class Product {
     public String getName() {
         return name;
     }
-
-    static public void add(ArrayList<Product> products) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название продукта");
-        String name = scanner.nextLine();
-        System.out.println("Введите кол-во продукта");
-        int count = scanner.nextInt();
-        System.out.println("Введите цену продукта");
-        int price = scanner.nextInt();
+    static public void add(ArrayList<Product> products, String name, int count, int price) {
         Product product = new Product(name, count, price);
         products.add(product);
         System.out.println("Ваша корзина" + (products));
     }
-
-    static public void remove(ArrayList<Product> products) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название продукта");
-
-        String name = scanner.nextLine();
+    static public void remove(ArrayList<Product> products, String name) {
         for (Product product : products) {
-
-            String nameChecked = product.getName();
-            if (nameChecked.equals(name)) {
+            if (product.getName().equals(name)) {
                 products.remove(product);
                 System.out.println("Успешно удалено");
                 System.out.println("Ваша корзина" + (products));
                 break;
             } else {
                 System.out.println("Соре соре продукт не найден");
-
             }
-
-
         }
     }
 
